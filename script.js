c95952ed8e9286d628d0cf282b07bcc8f500460e
ABAY2006 <abayssuresh@gmail.com>
2026-03-13 12:33:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,86 +1,59 @@
-const API = "https://backend1-0um3.onrender.com";
+const API = "http://127.0.0.1:5000";
 
-// SIGNUP
 async function signup(){
 
-const username=document.getElementById("su_user").value
-const password=document.getElementById("su_pass").value
+const username = document.getElementById("su_user").value
+const password = document.getElementById("su_pass").value
 
 if(!username || !password){
 alert("Enter username and password")
 return
 }
 
-const res=await fetch(API+"/signup",{
+try{
+
+const res = await fetch(API + "/signup",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
 },
-body:JSON.stringify({username,password})
+body:JSON.stringify({
+username:username,
+password:password
+})
 })
 
-const data=await res.json()
+const data = await res.json()
 
 alert(data.message)
 
+}catch(error){
+
+alert("Signup failed")
+console.log(error)
+
 }
 
-// LOGIN
+}
+
 async function login(){
 
-const username=document.getElementById("li_user").value
-const password=document.getElementById("li_pass").value
+const username = document.getElementById("li_user").value
+const password = document.getElementById("li_pass").value
 
-const res=await fetch(API+"/login",{
+const res = await fetch(API + "/login",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
 },
-body:JSON.stringify({username,password})
+body:JSON.stringify({
+username:username,
+password:password
+})
 })
 
-const data=await res.json()
+const data = await res.json()
 
 alert(data.message)
-
-}
-
-// IDENTIFY PLANT
-async function identifyPlant(){
-
-const file=document.getElementById("plantImage").files[0]
-
-if(!file){
-alert("Please upload image")
-return
-}
-
-const formData=new FormData()
-formData.append("image",file)
-
-const res=await fetch(API+"/identify",{
-method:"POST",
-body:formData
-})
-
-const data=await res.json()
-
-if(data.error){
-document.getElementById("result").innerHTML=data.error
-return
-}
-
-document.getElementById("result").innerHTML=
-
-`
-<h3>Scientific Name</h3>
-<p>${data.scientific}</p>
-
-<h3>Description</h3>
-<p>${data.description}</p>
-
-<h3>Medicinal Uses</h3>
-<p>${data.uses}</p>
-`
 
 }
